de5df021174746aab3549717eb58adcfb3edf8d1
Tom Anderson <dante.renevo@gmail.com>
2019-12-17 21:52:48 -0800

    Added remaining go base types to settings
    Added support for time.Duration
    
    Added support for custom settings via interface implementations

--- setting.go
+++ setting.go
@@ -4,7 +4,23 @@ import (
 	"fmt"
 	"strconv"
 	"sync"
+	"time"
 )
+
+// Marshaler is the interface implemented by types that can marshal themselves into a setting string.
+type Marshaler interface {
+	MarshalSetting() string
+}
+
+// Unmarshaler is the interface implemented by types that can unmarshal a string setting of themselves.
+type Unmarshaler interface {
+	UnmarshalSetting(string) error
+}
+
+// Equality is the interface implemented by type to validate equality of the supplied string to themselves.
+type Equality interface {
+	Equals(string) bool
+}
 
 // Value is an interface for interacting with the underlying configuration value
 type Value interface{}
@@ -51,6 +67,7 @@ func (s *Setting) Set(v string) error {
 			return fmt.Errorf("unable to cast value to boolean: %w", err)
 		}
 		*val = pv
+
 	case int:
 		pv, err := strconv.ParseInt(v, 0, strconv.IntSize)
 		if err != nil {
@@ -63,6 +80,55 @@ func (s *Setting) Set(v string) error {
 			return fmt.Errorf("unable to cast value to int: %w", err)
 		}
 		*val = int(pv)
+	case int8:
+		pv, err := strconv.ParseInt(v, 0, 8)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to int8: %w", err)
+		}
+		s.Value = int8(pv)
+	case *int8:
+		pv, err := strconv.ParseInt(v, 0, 8)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to int8: %w", err)
+		}
+		*val = int8(pv)
+	case int16:
+		pv, err := strconv.ParseInt(v, 0, 16)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to int16: %w", err)
+		}
+		s.Value = int16(pv)
+	case *int16:
+		pv, err := strconv.ParseInt(v, 0, 16)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to int16: %w", err)
+		}
+		*val = int16(pv)
+	case int32:
+		pv, err := strconv.ParseInt(v, 0, 32)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to int32: %w", err)
+		}
+		s.Value = int32(pv)
+	case *int32:
+		pv, err := strconv.ParseInt(v, 0, 32)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to int32: %w", err)
+		}
+		*val = int32(pv)
+	case int64:
+		pv, err := strconv.ParseInt(v, 0, 64)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to int64: %w", err)
+		}
+		s.Value = pv
+	case *int64:
+		pv, err := strconv.ParseInt(v, 0, 64)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to int64: %w", err)
+		}
+		*val = pv
+
 	case uint:
 		pv, err := strconv.ParseUint(v, 0, strconv.IntSize)
 		if err != nil {
@@ -75,8 +141,98 @@ func (s *Setting) Set(v string) error {
 			return fmt.Errorf("unable to case value to uint: %w", err)
 		}
 		*val = uint(pv)
+	case uint8:
+		pv, err := strconv.ParseUint(v, 0, 8)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to uint8: %w", err)
+		}
+		s.Value = uint8(pv)
+	case *uint8:
+		pv, err := strconv.ParseUint(v, 0, 8)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to uint8: %w", err)
+		}
+		*val = uint8(pv)
+	case uint16:
+		pv, err := strconv.ParseUint(v, 0, 16)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to uint16: %w", err)
+		}
+		s.Value = uint16(pv)
+	case *uint16:
+		pv, err := strconv.ParseUint(v, 0, 16)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to uint16: %w", err)
+		}
+		*val = uint16(pv)
+	case uint32:
+		pv, err := strconv.ParseUint(v, 0, 32)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to uint32: %w", err)
+		}
+		s.Value = uint32(pv)
+	case *uint32:
+		pv, err := strconv.ParseUint(v, 0, 32)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to uint32: %w", err)
+		}
+		*val = uint32(pv)
+	case uint64:
+		pv, err := strconv.ParseUint(v, 0, 64)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to uint64: %w", err)
+		}
+		s.Value = pv
+	case *uint64:
+		pv, err := strconv.ParseUint(v, 0, 64)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to uint64: %w", err)
+		}
+		*val = pv
+
+	case float32:
+		pv, err := strconv.ParseFloat(v, 32)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to float32: %w", err)
+		}
+		s.Value = float32(pv)
+	case *float32:
+		pv, err := strconv.ParseFloat(v, 32)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to float32: %w", err)
+		}
+		*val = float32(pv)
+	case float64:
+		pv, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to float64: %w", err)
+		}
+		s.Value = pv
+	case *float64:
+		pv, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to float64: %w", err)
+		}
+		*val = pv
+
+	case time.Duration:
+		pv, err := time.ParseDuration(v)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to time.Duration: %w", err)
+		}
+		s.Value = pv
+	case *time.Duration:
+		pv, err := time.ParseDuration(v)
+		if err != nil {
+			return fmt.Errorf("unable to cast value to time.Duration: %w", err)
+		}
+		*val = pv
+
 	default:
-		// TODO: see if we have a MarshalSetting implementation
+		if unmarshaler, ok := s.Value.(Unmarshaler); ok {
+			return unmarshaler.UnmarshalSetting(v)
+		}
+
 		return fmt.Errorf("type %T not supported", s.Value)
 	}
 
@@ -97,16 +253,63 @@ func (s *Setting) String() string {
 		return strconv.FormatBool(val)
 	case *bool:
 		return strconv.FormatBool(*val)
+
 	case int:
 		return strconv.Itoa(val)
 	case *int:
 		return strconv.Itoa(*val)
+	case int8:
+		return strconv.FormatInt(int64(val), 10)
+	case *int8:
+		return strconv.FormatInt(int64(*val), 10)
+	case int16:
+		return strconv.FormatInt(int64(val), 10)
+	case *int16:
+		return strconv.FormatInt(int64(*val), 10)
+	case int32:
+		return strconv.FormatInt(int64(val), 10)
+	case *int32:
+		return strconv.FormatInt(int64(*val), 10)
+	case int64:
+		return strconv.FormatInt(val, 10)
+	case *int64:
+		return strconv.FormatInt(*val, 10)
+
 	case uint:
 		return strconv.FormatUint(uint64(val), 10)
 	case *uint:
 		return strconv.FormatUint(uint64(*val), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(val), 10)
+	case *uint8:
+		return strconv.FormatUint(uint64(*val), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(val), 10)
+	case *uint16:
+		return strconv.FormatUint(uint64(*val), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(val), 10)
+	case *uint32:
+		return strconv.FormatUint(uint64(*val), 10)
+	case uint64:
+		return strconv.FormatUint(val, 10)
+	case *uint64:
+		return strconv.FormatUint(*val, 10)
+
+	case float32:
+		return strconv.FormatFloat(float64(val), 'g', -1, 32)
+	case *float32:
+		return strconv.FormatFloat(float64(*val), 'g', -1, 32)
+	case float64:
+		return strconv.FormatFloat(val, 'g', -1, 64)
+	case *float64:
+		return strconv.FormatFloat(*val, 'g', -1, 64)
+
 	default:
-		// TODO: see if we have an UnmarshalSetting implementation
+		if marshaler, ok := s.Value.(Marshaler); ok {
+			return marshaler.MarshalSetting()
+		}
+
 		return fmt.Sprintf("%v", val)
 	}
 }
@@ -130,6 +333,7 @@ func (s *Setting) Equals(v string) bool {
 			return false
 		}
 		return *val == pv
+
 	case int:
 		pv, err := strconv.ParseInt(v, 0, strconv.IntSize)
 		if err != nil {
@@ -142,6 +346,55 @@ func (s *Setting) Equals(v string) bool {
 			return false
 		}
 		return *val == int(pv)
+	case int8:
+		pv, err := strconv.ParseInt(v, 0, 8)
+		if err != nil {
+			return false
+		}
+		return val == int8(pv)
+	case *int8:
+		pv, err := strconv.ParseInt(v, 0, 8)
+		if err != nil {
+			return false
+		}
+		return *val == int8(pv)
+	case int16:
+		pv, err := strconv.ParseInt(v, 0, 16)
+		if err != nil {
+			return false
+		}
+		return val == int16(pv)
+	case *int16:
+		pv, err := strconv.ParseInt(v, 0, 16)
+		if err != nil {
+			return false
+		}
+		return *val == int16(pv)
+	case int32:
+		pv, err := strconv.ParseInt(v, 0, 32)
+		if err != nil {
+			return false
+		}
+		return val == int32(pv)
+	case *int32:
+		pv, err := strconv.ParseInt(v, 0, 32)
+		if err != nil {
+			return false
+		}
+		return *val == int32(pv)
+	case int64:
+		pv, err := strconv.ParseInt(v, 0, 64)
+		if err != nil {
+			return false
+		}
+		return val == pv
+	case *int64:
+		pv, err := strconv.ParseInt(v, 0, 64)
+		if err != nil {
+			return false
+		}
+		return *val == pv
+
 	case uint:
 		pv, err := strconv.ParseUint(v, 0, strconv.IntSize)
 		if err != nil {
@@ -154,8 +407,99 @@ func (s *Setting) Equals(v string) bool {
 			return false
 		}
 		return *val == uint(pv)
+	case uint8:
+		pv, err := strconv.ParseUint(v, 0, 8)
+		if err != nil {
+			return false
+		}
+		return val == uint8(pv)
+	case *uint8:
+		pv, err := strconv.ParseUint(v, 0, 8)
+		if err != nil {
+			return false
+		}
+		return *val == uint8(pv)
+	case uint16:
+		pv, err := strconv.ParseUint(v, 0, 16)
+		if err != nil {
+			return false
+		}
+		return val == uint16(pv)
+	case *uint16:
+		pv, err := strconv.ParseUint(v, 0, 16)
+		if err != nil {
+			return false
+		}
+		return *val == uint16(pv)
+	case uint32:
+		pv, err := strconv.ParseUint(v, 0, 32)
+		if err != nil {
+			return false
+		}
+		return val == uint32(pv)
+	case *uint32:
+		pv, err := strconv.ParseUint(v, 0, 32)
+		if err != nil {
+			return false
+		}
+		return *val == uint32(pv)
+	case uint64:
+		pv, err := strconv.ParseUint(v, 0, 64)
+		if err != nil {
+			return false
+		}
+		return val == pv
+	case *uint64:
+		pv, err := strconv.ParseUint(v, 0, 64)
+		if err != nil {
+			return false
+		}
+		return *val == pv
+
+	case float32:
+		pv, err := strconv.ParseFloat(v, 32)
+		if err != nil {
+			return false
+		}
+		return val == float32(pv)
+	case *float32:
+		pv, err := strconv.ParseFloat(v, 32)
+		if err != nil {
+			return false
+		}
+		return *val == float32(pv)
+
+	case float64:
+		pv, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return false
+		}
+		return val == pv
+	case *float64:
+		pv, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return false
+		}
+		return *val == pv
+
+	case time.Duration:
+		pv, err := time.ParseDuration(v)
+		if err != nil {
+			return false
+		}
+		return val == pv
+	case *time.Duration:
+		pv, err := time.ParseDuration(v)
+		if err != nil {
+			return false
+		}
+		return *val == pv
+
 	default:
-		// TODO: see if we have an Equals implementation
+		if equality, ok := s.Value.(Equality); ok {
+			return equality.Equals(v)
+		}
+
 		return fmt.Sprintf("%v", val) == v
 	}
 }
--- setting_test.go
+++ setting_test.go
@@ -1,14 +1,17 @@
 package config
 
 import (
+	"bytes"
 	"fmt"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type setTest struct {
 	To          interface{}
 	Initializer interface{}
+	CheckString string
 }
 
 func (s setTest) Value() interface{} {
@@ -19,45 +22,142 @@ func (s setTest) Equals(v interface{}) bool {
 	return reflect.Indirect(reflect.ValueOf(s.Initializer)).Interface() == v
 }
 
-func newSetTest(to interface{}, v interface{}) setTest {
+func newSetTest(to interface{}, v interface{}, s string) setTest {
 	p := reflect.New(reflect.TypeOf(v))
 	p.Elem().Set(reflect.ValueOf(v))
 
 	return setTest{
 		To:          to,
 		Initializer: p.Interface(),
+		CheckString: s,
 	}
 }
 
 func TestSetting_Set(t *testing.T) {
 	tests := []setTest{
-		newSetTest("changed", "initial"),
-		newSetTest(true, false),
+		newSetTest("changed", "initial", "initial"),
+		newSetTest(true, false, "False"),
+		newSetTest(time.Minute*23, time.Minute, "1m"),
 
-		newSetTest(int(23), int(5)),
-		newSetTest(int8(23), int8(5)),
-		newSetTest(int16(23), int16(5)),
-		newSetTest(int32(23), int32(5)),
-		newSetTest(int64(23), int64(5)),
+		newSetTest(int(23), int(5), "5"),
+		newSetTest(int8(23), int8(5), "5"),
+		newSetTest(int16(23), int16(5), "5"),
+		newSetTest(int32(23), int32(5), "5"),
+		newSetTest(int64(23), int64(5), "5"),
 
-		newSetTest(uint(23), uint(5)),
-		newSetTest(uint8(23), uint8(5)),
-		newSetTest(uint16(23), uint16(5)),
-		newSetTest(uint32(23), uint32(5)),
-		newSetTest(uint64(23), uint64(5)),
+		newSetTest(uint(23), uint(5), "5"),
+		newSetTest(uint8(23), uint8(5), "5"),
+		newSetTest(uint16(23), uint16(5), "5"),
+		newSetTest(uint32(23), uint32(5), "5"),
+		newSetTest(uint64(23), uint64(5), "5"),
+
+		newSetTest(float32(23), float32(5), "5"),
+		newSetTest(float64(23), float64(5), "5"),
+
+		// actually treated like a uint8, but we make sure it works
+		newSetTest(byte(6), byte(5), "5"),
 	}
 
 	for _, test := range tests {
-		t.Run(fmt.Sprintf("%T", test.To), func(t *testing.T) {
+		testName := fmt.Sprintf("%T", test.To)
+
+		// override the internal type resolutions to test type aliases
+		if v, ok := test.To.(byte); ok && v == 6 {
+			testName = "byte"
+		}
+
+		t.Run(testName, func(t *testing.T) {
 			s := &Setting{Value: test.Initializer}
 
+			// validates if the provided string matches the formatting of the string value
+			if !s.Equals(test.CheckString) {
+				t.Errorf("Failed to equality check supplied string value: expected %q; got %q", test.CheckString, s.String())
+			}
+
+			// make sure we can set the value from the provided string value
+			if err := s.Set(test.CheckString); err != nil {
+				t.Errorf("Failed to set from string value: %v", err)
+			}
+
+			// make sure we can set the value from the raw sprinted to string
 			if err := s.Set(fmt.Sprintf("%v", test.To)); err != nil {
 				t.Fatalf("Failed to set string value: %v", err)
 			}
 
+			// validate that the pointer was in fact changed to the new value
 			if !test.Equals(test.To) {
 				t.Errorf("Failed to update value: expected %v; got %v", test.To, test.Value())
 			}
+
+			// validate that we don't get a blank string back (could probably be a better test TBH)
+			if s.String() == "" {
+				t.Errorf("Failed to string value: got %q", s.String())
+			}
+
+			// validate that the fmt.sprintf matches the equality checker
+			if !s.Equals(fmt.Sprintf("%v", test.To)) {
+				t.Errorf("Failed to equality check string value: expected %q; got %q", fmt.Sprintf("%v", test.To), s.String())
+			}
 		})
+	}
+}
+
+type customSetting struct {
+	Value       []byte
+	Marshaled   bool
+	Unmarshaled bool
+	Equaled     bool
+}
+
+func (cs *customSetting) UnmarshalSetting(v string) error {
+	cs.Value = []byte(v)
+	cs.Unmarshaled = true
+	return nil
+}
+
+func (cs *customSetting) MarshalSetting() string {
+	cs.Marshaled = true
+	return string(cs.Value)
+}
+
+func (cs *customSetting) Equals(v string) bool {
+	cs.Equaled = true
+	return bytes.Equal(cs.Value, []byte(v))
+}
+
+func TestSetting_CustomType(t *testing.T) {
+	cs := &customSetting{
+		Value: []byte("hello"),
+	}
+
+	st := &Setting{Value: cs}
+
+	if string(cs.Value) != st.String() {
+		t.Errorf("Failed to get string value for custom type")
+	}
+	if !cs.Marshaled {
+		t.Error("Custom object MarshalSetting not called")
+	}
+
+	newValue := "goodbye"
+
+	if err := st.Set(newValue); err != nil {
+		t.Fatalf("Failed to set string value for custom type: %v", err)
+	}
+
+	if !cs.Unmarshaled {
+		t.Errorf("Custom object UnmarshalSetting not called")
+	}
+
+	if string(cs.Value) != newValue {
+		t.Errorf("Failed to get updated string value for custom type: expected %q; got %q", newValue, string(cs.Value))
+	}
+
+	if !st.Equals(newValue) {
+		t.Error("Failed to match equality on setting to set value")
+	}
+
+	if !cs.Equaled {
+		t.Errorf("Custom object Equals not called")
 	}
 }
